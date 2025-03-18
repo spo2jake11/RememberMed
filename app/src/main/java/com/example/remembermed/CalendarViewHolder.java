a5e22225d800a628762c0920db6046c584aa597d
Jake Villanueva <spo2jake11@gmail.com>
2025-03-18 19:38:28 +0800

    0.0.5 -- Added Weekly View

--- app/src/main/java/com/example/remembermed/CalendarViewHolder.java
+++ app/src/main/java/com/example/remembermed/CalendarViewHolder.java
@@ -7,11 +7,14 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public final View parentView;
     public final TextView dayOfMonth;
     private final CalendarAdapter.OnItemListener onItemListener;
     public CalendarViewHolder(@NonNull View itemView, CalendarAdapter.OnItemListener onItemListener)
     {
         super(itemView);
+        parentView = itemView.findViewById(R.id.parentView);
         dayOfMonth = itemView.findViewById(R.id.txtCell);
         this.onItemListener = onItemListener;
         itemView.setOnClickListener(this);
--- app/src/main/java/com/example/remembermed/WeeklyViewActivity.java
+++ app/src/main/java/com/example/remembermed/WeeklyViewActivity.java
@@ -1,10 +1,10 @@
 package com.example.remembermed;
 
 import static com.example.remembermed.ui.theme.CalendarUtils.daysInMonthArray;
+import static com.example.remembermed.ui.theme.CalendarUtils.daysInWeekArray;
 import static com.example.remembermed.ui.theme.CalendarUtils.monthYearFromDate;
 import static com.example.remembermed.ui.theme.CalendarUtils.selectedDate;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -12,72 +12,69 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.helper.widget.Grid;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.remembermed.ui.theme.CalendarUtils;
-
 import java.time.LocalDate;
-import java.time.YearMonth;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener{
-
+public class WeeklyViewActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener{
 
     private TextView txtMonthYear;
     private RecyclerView calendarRecView;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_weekly_view);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         initWidgets();
-        selectedDate = LocalDate.now();
-        setMonthView();
+        setWeekView();
     }
 
-    private void setMonthView() {
+    private void setWeekView() {
         txtMonthYear.setText(monthYearFromDate(selectedDate));
-        ArrayList<LocalDate> daysInMonth = daysInMonthArray(selectedDate);
+        ArrayList<LocalDate> days = daysInWeekArray(selectedDate);
 
-        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
+        CalendarAdapter calendarAdapter = new CalendarAdapter(days, this);
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
         calendarRecView.setLayoutManager(layoutManager);
         calendarRecView.setAdapter(calendarAdapter);
     }
+
+
 
     private void initWidgets() {
         calendarRecView = findViewById(R.id.calendarRecView);
         txtMonthYear = findViewById(R.id.txtMonthDay);
     }
 
-    public void prevMonthAction(View view) {
-        selectedDate = selectedDate.minusMonths(1);
-        setMonthView();
+    public void prevWeekAction(View view) {
+        selectedDate = selectedDate.minusWeeks(1);
+        setWeekView();
     }
 
-    public void nextMonthAction(View view) {
-        selectedDate = selectedDate.plusMonths(1);
-        setMonthView();
+    public void nextWeekAction(View view) {
+        selectedDate = selectedDate.plusWeeks(1);
+        setWeekView();
+    }
+
+    public void newEventAction(View view) {
+
     }
 
     @Override
     public void onItemClick(int position, String dayText) {
-        if(dayText.equals("")){
             String message = "Selected date " + dayText + " " + monthYearFromDate(selectedDate);
             Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-        }
-    }
-
-    public void weeklyAction(View view) {
-        startActivity(new Intent(this, WeeklyViewActivity.class));
     }
 }
